10c1292dfeb75f2837d30c609899d1821543018e
Sankalp Pradhan <sankalppradhan1906@gmail.com>
2026-01-20 20:41:18 +0530

    refractered project update

--- src/features/projects/hooks/use-project-id.ts
+++ src/features/projects/hooks/use-project-id.ts
@@ -1,7 +1,8 @@
+"use client"
+
 import { useParams } from "next/navigation";
 
 export const useProjectId = () => {
     const params = useParams();
-    // This must match the folder name in your (dashboard) or [projectId] route
     return params.projectId as string;
 };
