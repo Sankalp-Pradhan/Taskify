9dc67d8c5dc674b358e4699e32b6a70cfe3cbf5c
Sankalp Pradhan <sankalppradhan1906@gmail.com>
2026-01-21 22:38:33 +0530

    added :  vecel json

--- src/features/auth/schemas.ts
+++ src/features/auth/schemas.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-    email: z.email(),
+    email: z.string().email(),
     password: z.string().min(1, "Required").max(256),
 });
 
 
 export const registerSchema = z.object({
     name: z.string().trim().min(1, "Required"),
-    email: z.email(),
+    email: z.string().email(),
     password: z.string().min(8, "Minimum of 8 characters").max(256),
 });
